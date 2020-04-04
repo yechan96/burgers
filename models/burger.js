4511a84dfc6d8aed53c5d38a644cde6e6fd26461
yechan96 <yechanlee96@gmail.com>
2020-04-04 13:31:30 -0700

    model

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm");
+
+var burger = {
+  all: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+module.exports = burger;
